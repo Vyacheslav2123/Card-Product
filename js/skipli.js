b67fdeed97cfefd3a7fa8a705c5008108f4f3f72
Vyacheslav2123 <72310551+Vyacheslav2123@users.noreply.github.com>
2022-03-29 17:52:52 +0300

    Update skipli.js

--- js/skipli.js
+++ js/skipli.js
@@ -15,14 +15,16 @@ let ProductDetailPageBySkipli = function (event) {
         }
     }
     if (event.type == "click") {
-        let top = 60;
-        if (event.target.classList.contains("images-list__arrow-top")) top = -60;
+        let top = 0;
+        if (event.target.classList.contains("images-list__arrow-bottom")) top = 60;
+        else if (event.target.classList.contains("images-list__arrow-top")) top = -60;
         imgContainer.scrollBy({
             top,
             behavior: 'smooth'
         })
         let left = -70;
-        if (event.target.classList.contains("images-list__arrow-top")) left = 70;
+        if (event.target.classList.contains("images-list__arrow-bottom")) left = -70;
+        else if (event.target.classList.contains("images-list__arrow-top")) left = 70;
         imgContainer.scrollLeft += left;
     }
 };

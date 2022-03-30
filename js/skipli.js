e4f4d94374238cad65a5d7845f224f4a6f3c7bee
Vyacheslav2123 <72310551+Vyacheslav2123@users.noreply.github.com>
2022-03-30 18:47:00 +0300

    Update skipli.js

--- js/skipli.js
+++ js/skipli.js
@@ -13,33 +13,33 @@
 
 	let ProductDetailPageBySkipli = function (event) {
 	    if (event.type == "mouseover" || event.type == "click") {
-		if (event.target.classList.contains("images-list-img")) {
+		if (event.target.classList.contains("carusel-v2__images-list-img")) {
 		    Skipli_currentPosition = Skipli_getIndex(event.target);
 		    Skipli_changePopupImg(0);
 		    Skipli_images.forEach((item) => {
-			item.classList.remove("active");
+			item.classList.remove("carusel-v2__active");
 		    });
-		    event.target.parentElement.classList.add("active");
+		    event.target.parentElement.classList.add("carusel-v2__active");
 		}
 	    }
 	    if (event.type == "click") {
-		if(event.target.classList.contains("carusel-v2-arrow-right")) Skipli_changePopupImg(1);
-		else if(event.target.classList.contains("carusel-v2-arrow-left")) Skipli_changePopupImg(-1);
-		if(event.target.classList.contains("images-list-img") || event.target.classList.contains("carusel-v2__important-img")){
+		if(event.target.classList.contains("carusel-v2__carusel-v2-arrow-right")) Skipli_changePopupImg(1);
+		else if(event.target.classList.contains("carusel-v2__carusel-v2-arrow-left")) Skipli_changePopupImg(-1);
+		if(event.target.classList.contains("carusel-v2__images-list-img") || event.target.classList.contains("carusel-v2__important-img")){
 		    Skipli_currentPosition = Skipli_getIndex(Skipli_getChild(document.querySelector(".carusel-v2__images-list-img-container.active")));
 		    Skipli_togglePopup(true);
 		    Skipli_popupImg.src = event.target.src;
 		}
 		let top = 0;
-		if (event.target.classList.contains("images-list__arrow-bottom")) top = 60;
-		else if (event.target.classList.contains("images-list__arrow-top")) top = -60;
+		if (event.target.classList.contains("carusel-v2__images-list__arrow-bottom")) top = 60;
+		else if (event.target.classList.contains("carusel-v2__images-list__arrow-top")) top = -60;
 		Skipli_imgContainer.scrollBy({
 		    top,
 		    behavior: 'smooth'
 		});
 		let left = -1;
-		if (event.target.classList.contains("images-list__arrow-bottom")) left = -1;
-		else if (event.target.classList.contains("images-list__arrow-top")) left = 1;
+		if (event.target.classList.contains("carusel-v2__images-list__arrow-bottom")) left = -1;
+		else if (event.target.classList.contains("carusel-v2__images-list__arrow-top")) left = 1;
 		Skipli_imgContainer.scrollLeft += left * 70;
 	    }
 	};
@@ -50,10 +50,10 @@
 	    Skipli_popupImg.src = Skipli_getChild(Array.prototype.slice.call(Skipli_imgContainerPopup.childNodes).filter(e => !(e instanceof Text))[Skipli_currentPosition]).src;
 	    Skipli_glImg.src = Skipli_popupImg.src;
 	    Skipli_images.forEach((item) => {
-		item.classList.remove("active");
+		item.classList.remove("carusel-v2__active");
 	    });
-	    Array.prototype.slice.call(Skipli_imgContainerPopup.childNodes).filter(e => !(e instanceof Text))[Skipli_currentPosition]?.classList?.add("active");
-	    Array.prototype.slice.call(Skipli_imgContainer.childNodes).filter(e => !(e instanceof Text))[Skipli_currentPosition]?.classList?.add("active");
+	    Array.prototype.slice.call(Skipli_imgContainerPopup.childNodes).filter(e => !(e instanceof Text))[Skipli_currentPosition]?.classList?.add("carusel-v2__active");
+	    Array.prototype.slice.call(Skipli_imgContainer.childNodes).filter(e => !(e instanceof Text))[Skipli_currentPosition]?.classList?.add("carusel-v2__active");
 	    document.querySelector(".carusel-v2__carusel-v2-popup-index").innerText = (Skipli_currentPosition + 1) + " из " + (Skipli_maxCurrentPosition + 1);
 	    Skipli_imgContainerPopup.scroll({
 		left: (Skipli_currentPosition * Skipli_getOneWidth()) - (window.innerWidth / 2) + (Skipli_getOneWidth() / 2),
@@ -69,11 +69,11 @@
 	function Skipli_togglePopup(open = true){
 	    if(open) {
 		setTimeout(SkipliOnResize, 100);
-		Skipli_popup.classList.add("carusel-v2-popup-view");
+		Skipli_popup.classList.add("carusel-v2__carusel-v2-popup-view");
 		document.body.style.position = "fixed";
 	    }
 	    else {
-		Skipli_popup.classList.remove("carusel-v2-popup-view");
+		Skipli_popup.classList.remove("carusel-v2__carusel-v2-popup-view");
 		document.body.style.position = "relative";
 	    }
 	}
